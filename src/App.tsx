47b9c91a4f2432cc4085c8f5f6752015a534f148
Marcello <marcello_gp_@hotmail.com>
2023-02-06 17:06:45 -0300

    refactor: removed css import unused and deleted css unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
 import { GlobalStyles } from "./styles/global";
