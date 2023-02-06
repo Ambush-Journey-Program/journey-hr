44cbf2f16a27d6b9eb89a2b6e2c6c529c06310ef
Afonso Ferrer <aferrer1@whopper.com>
2023-02-06 01:44:10 +0100

    Added first component in design-system folder

---
+++ src/design-system/mock-component/mock-component.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react'
+
+import { Wrapper } from './mock-component.styled'
+import { IMockComponentProps } from './types'
+
+export const MockComponent: FC<IMockComponentProps> = ({ label }) => {
+  return <Wrapper>{label}</Wrapper>
+}
