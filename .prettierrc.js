7dc7b4e093cb66acb59fcca7750d622f7208cf9a
José Ferreira <dev.josejunior@gmail.com>
2023-02-07 17:48:45 -0300

    fix eslint e prettier config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
+  "semi": true,
   "trailingComma": "all",
   "tabWidth": 2,
-  "semi": true,
   "singleQuote": true,
-  "printWidth": 120,
+  "printWidth": 80,
   "bracketSpacing": true
 }
