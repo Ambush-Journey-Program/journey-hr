f126b457083fb7920a1117610b32ceb56a8da48d
Lucas Medeiros <lucaspontoe@hotmail.com>
2023-02-08 14:33:06 -0300

    feat: implement variation of size to the button

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,37 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Button onClick={()=>{console.log("teste")}} disabled>Label</Button>
+      <Button
+        onClick={() => {
+          console.log("teste");
+        }}
+        sizeVariant="small"
+      >
+        Label
+      </Button>
+      <Button
+        onClick={() => {
+          console.log("teste");
+        }}
+      >
+        Label
+      </Button>
+      <Button
+        onClick={() => {
+          console.log("teste");
+        }}
+        sizeVariant="large"
+      >
+        Label
+      </Button>
+      <Button
+        onClick={() => {
+          console.log("teste");
+        }}
+        disabled
+      >
+        Label
+      </Button>
     </ThemeProvider>
   );
 }
--- src/design-system/button/button.styled.ts
+++ src/design-system/button/button.styled.ts
@@ -1,43 +1,52 @@
-import styled, { css, FlattenSimpleInterpolation } from 'styled-components'
+import styled, { css, FlattenSimpleInterpolation } from "styled-components";
 
 type StyleProps = {
-    sizeVariant: string;
-}
-
+  sizeVariant: string;
+};
 
 type Variants = {
-    [key: string]: FlattenSimpleInterpolation;
-}
+  [key: string]: FlattenSimpleInterpolation;
+};
 
-const variants:Variants = {
-    default: css`
-        width: 94px;
-        height: 47px;
-        font-size: 18px;
-    `,
-  };
+const variants: Variants = {
+  large: css`
+    width: 104px;
+    height: 53px;
+    font-size: 22px;
+  `,
 
-  const disabledButton = css`
-   background-color: ${props => props.theme.color.disable};
-   &:hover{
+  default: css`
+    width: 94px;
+    height: 47px;
+    font-size: 18px;
+  `,
+
+  small: css`
+    width: 89px;
+    height: 39px;
+    font-size: 16px;
+  `,
+};
+
+const disabledButton = css`
+  background-color: ${(props) => props.theme.color.disable};
+  &:hover {
     opacity: 1;
-   }
-  `;
-
-export const ButtonStyle = styled.button<StyleProps>`
-    font-weight: 600;
-    line-height: 150%;
-    font-family: Mundial, sans-serif;
-    color: ${props => props.theme.color.white};
-    background-color: ${props => props.theme.color.red};
-    border-radius: 50px;
-
-    &:hover{
-        opacity: 70%;
-    }
-
-    ${({ sizeVariant }) => variants[sizeVariant]}
-    ${({disabled}) => disabled && disabledButton}
+  }
 `;
 
+export const ButtonStyle = styled.button<StyleProps>`
+  font-weight: 600;
+  line-height: 150%;
+  font-family: Mundial, sans-serif;
+  color: ${(props) => props.theme.color.white};
+  background-color: ${(props) => props.theme.color.red};
+  border-radius: 50px;
 
+  &:hover {
+    opacity: 70%;
+  }
+
+  ${({ sizeVariant }) => variants[sizeVariant]}
+  ${({ disabled }) => disabled && disabledButton}
+`;
