39e9dadc22bce9a8ab2d7fc08b016e94f784edbd
Lucas Medeiros <lucaspontoe@hotmail.com>
2023-02-09 10:50:50 -0300

    fix: cleaned app.ts 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
 import { GlobalStyles } from "./styles/global";
-import { Button } from "./design-system/button";
-import { ContainerInput } from "./design-system/container-input";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <ContainerInput></ContainerInput>
     </ThemeProvider>
   );
 }
