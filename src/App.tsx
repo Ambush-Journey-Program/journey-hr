9ad321849b3c22392b54abe77946cb0c41606969
Mariana Maia <marianalima.amb@gmail.com>
2023-02-09 18:23:18 -0300

    fix: cleaned the app.tsx by deleting the input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { ThemeProvider } from "styled-components"
 import { theme } from "./styles/theme"
 import { GlobalStyles } from "./styles/global"
-import { Button } from "./design-system/button"
-import { Input } from "./design-system/input/input"
 
 function App() {
   return (
