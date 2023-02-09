5dee6363740ff5b7d23867e56e040cba4be4899f
Lucas Medeiros <lucaspontoe@hotmail.com>
2023-02-09 10:48:25 -0300

    fix: cleaned app.ts

--- src/App.tsx
+++ src/App.tsx
@@ -2,42 +2,13 @@ import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
 import { GlobalStyles } from "./styles/global";
 import { Button } from "./design-system/button";
+import { ContainerInput } from "./design-system/container-input";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Button
-        onClick={() => {
-          console.log("teste");
-        }}
-        sizeVariant="small"
-      >
-        Label
-      </Button>
-      <Button
-        onClick={() => {
-          console.log("teste");
-        }}
-      >
-        Label
-      </Button>
-      <Button
-        onClick={() => {
-          console.log("teste");
-        }}
-        sizeVariant="large"
-      >
-        Label
-      </Button>
-      <Button
-        onClick={() => {
-          console.log("teste");
-        }}
-        disabled
-      >
-        Label
-      </Button>
+      <ContainerInput></ContainerInput>
     </ThemeProvider>
   );
 }
