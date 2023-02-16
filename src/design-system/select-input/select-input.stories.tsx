6eac962c5b05e73ccc6be326d6f3b7848665ba2b
Mariana Maia <marianalima.amb@gmail.com>
2023-02-16 16:21:42 -0300

    docs: fix select-input story

--- src/design-system/select-input/select-input.stories.tsx
+++ src/design-system/select-input/select-input.stories.tsx
@@ -1,5 +1,8 @@
 import { Meta, Story } from '@storybook/react';
 
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../../styles/theme';
+
 import { SelectInput } from './select-input';
 import { ISelectInputProps } from './types';
 
@@ -12,8 +15,32 @@ export default {
   },
 } as Meta;
 
+const exampleOptions = [
+  {
+    label: 'Text 1',
+    value: '1',
+  },
+  {
+    label: 'Text 2',
+    value: '2',
+  },
+  {
+    label: 'Text 3',
+    value: '3',
+  },
+];
+
 const Template: Story<ISelectInputProps> = (args) => {
-  return <SelectInput {...args} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <SelectInput {...args} />
+    </ThemeProvider>
+  );
 };
 
 export const SelectInputStory = Template.bind({});
+SelectInputStory.args = {
+  title: 'Title Example',
+  required: true,
+  options: exampleOptions,
+};
