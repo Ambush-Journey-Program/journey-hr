429f866d17e8229f1ba2b6ef5655869d669ebc23
Mariana Maia <marianalima.amb@gmail.com>
2023-02-16 16:11:34 -0300

    chore: fix namespace name

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,11 @@
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import { GlobalStyles } from './styles/global';
-import { SelectInput } from './design-system/select-input';
-
-const options = [
-  {
-    label: 'Text 1',
-    value: '1',
-  },
-  {
-    label: 'Text 2',
-    value: '2',
-  },
-  {
-    label: 'Text 3',
-    value: '3',
-  },
-];
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SelectInput
-        error="Oops! An error occured"
-        title="Title"
-        options={options}
-        onSelect={(value) => alert(value)}
-        required
-      />
-      {/* <SelectInput required disabled title="Title" options={options} />
-      <SelectInput title="Title" options={options} /> */}
     </ThemeProvider>
   );
 }
