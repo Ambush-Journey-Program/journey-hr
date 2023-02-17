1f100ef5018f94fbefef2797ca096957692c657e
Mariana Maia <marianalima.amb@gmail.com>
2023-02-17 17:14:23 -0300

    refactor: formatted

--- src/design-system/select-input/select-input.test.tsx
+++ src/design-system/select-input/select-input.test.tsx
@@ -1,38 +1,36 @@
 import * as React from 'react';
 import { describe, expect } from 'vitest';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../../styles/theme';
 import { SelectInput } from './select-input';
-import { join } from 'path';
+
+const title = 'My title';
+
+const exampleOptions = [
+  {
+    label: 'Text 1',
+    value: '1',
+  },
+  {
+    label: 'Text 2',
+    value: '2',
+  },
+  {
+    label: 'Text 3',
+    value: '3',
+  },
+];
 
 describe('<SelectInput />', () => {
   it('has a title', () => {
-    const title = 'My title';
-
-    const exampleOptions = [
-      {
-        label: 'Text 1',
-        value: '1',
-      },
-      {
-        label: 'Text 2',
-        value: '2',
-      },
-      {
-        label: 'Text 3',
-        value: '3',
-      },
-    ];
-
     render(
       <ThemeProvider theme={theme}>
         <SelectInput options={exampleOptions} title={title} />
       </ThemeProvider>,
     );
     const inputEl = screen.getByText(title);
-    expect(inputEl).toBeInTheDocument();
+    expect(inputEl).toBeInTheDocument(title);
   });
 
   it('should have options', () => {
@@ -64,28 +62,12 @@ describe('<SelectInput />', () => {
   });
 
   it('should be disabled', () => {
-    const exampleOptions = [
-      {
-        label: 'Text 1',
-        value: '1',
-      },
-      {
-        label: 'Text 2',
-        value: '2',
-      },
-      {
-        label: 'Text 3',
-        value: '3',
-      },
-    ];
-
     render(
       <ThemeProvider theme={theme}>
         <SelectInput options={exampleOptions} title="title" disabled />
       </ThemeProvider>,
     );
 
-    // const inputEl = screen.getByText('title');
     const inputEl = screen.getByTestId('input-select-test');
     expect(inputEl).toBeDisabled();
     expect(inputEl).not.toBeEnabled();
