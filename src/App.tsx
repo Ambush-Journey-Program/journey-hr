284c86e5a13b6a79642c5169dc708ab0ea26b7c6
Mariana Maia <marianalima.amb@gmail.com>
2023-02-28 14:55:43 -0300

    Feat: created the tooltip top

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import { GlobalStyles } from './styles/global';
-import { Tooltip } from './design-system/tooltip';
-import { TooltipTarget } from './design-system/tooltip/tooltip.styled';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Tooltip text="Copied!" position="Top">
-        {' '}
-        <TooltipTarget></TooltipTarget>
-      </Tooltip>
     </ThemeProvider>
   );
 }
