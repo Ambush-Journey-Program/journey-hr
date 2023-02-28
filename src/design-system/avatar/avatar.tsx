fdef956525f6c9188f87f9a9fe65ecf946e3d1fb
Lucas Medeiros <lucaspontoe@hotmail.com>
2023-02-28 12:12:08 -0300

    Feat: Created the basic avatar structure

---
+++ src/design-system/avatar/avatar.tsx
@@ -0,0 +1,10 @@
+import * as Styled from './avatar.styled';
+import { AvatarProps } from './types';
+
+export function Avatar({ ...props }: AvatarProps) {
+  return (
+    <>
+      <Styled.Image data-testid="Avatar" {...props} />
+    </>
+  );
+}
---
+++ src/design-system/avatar/types.ts
@@ -0,0 +1,4 @@
+import { ImgHTMLAttributes } from "react";
+
+export type AvatarProps = {
+  } & ImgHTMLAttributes<HTMLImageElement>;
