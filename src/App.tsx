b34907d79e3b43faf3e9e40435a37bd957acad40
Mariana Maia <marianalima.amb@gmail.com>
2023-03-01 15:30:11 -0300

    Feat: configured stylebook

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import { GlobalStyles } from './styles/global';
-import { Tooltip } from './design-system/tooltip';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Tooltip text="Copied asudhuashduhasuhuahsuhdsuhu!" position="right">
-        <button>Right</button>
-      </Tooltip>
-      <Tooltip text="Copied asudhuashduhasuhuahsuhdsuhu!" position="left">
-        <button>Left</button>
-      </Tooltip>
-      <Tooltip text="Copied asudhuashduhasuhuahsuhdsuhu!" position="bottom">
-        <button>Bottom</button>
-      </Tooltip>
-      <Tooltip text="Copied asudhuashduhasuhuahsuhdsuhu!" position="top">
-        <button>Top</button>
-      </Tooltip>
-
-      <Tooltip text="Copied asudhuashduhasuhuahsuhdsuhu!">
-        <button>Padrão</button>
-      </Tooltip>
     </ThemeProvider>
   );
 }
--- src/design-system/tooltip/tooltip.stories.tsx
+++ src/design-system/tooltip/tooltip.stories.tsx
@@ -1,24 +1,32 @@
 import { Meta, Story } from '@storybook/react';
-
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../../styles/theme';
 import { Tooltip } from './tooltip';
+
 import { ITooltipProps } from './types';
 
 export default {
   component: Tooltip,
   title: 'UI/Tooltip',
   parameters: {
-    isLoading: false,
-    // label: 'Tooltip text',
-    children: 'Label',
+    children: <p>Ambush</p>,
+    position: '',
+    text: 'Copied!',
   },
 } as Meta;
 
 const Template: Story<ITooltipProps> = (args) => {
-  return <Tooltip {...args} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Tooltip {...args} />
+    </ThemeProvider>
+  );
 };
 
 export const TooltipStory = Template.bind({});
 
 TooltipStory.args = {
-  children: 'Label',
+  children: <p>Ambush</p>,
+  position: 'top',
+  text: 'Copied!',
 };
