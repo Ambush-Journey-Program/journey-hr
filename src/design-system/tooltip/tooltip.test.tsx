03f2ae603205067417917311109b8bfced2bb124
Mariana Maia <marianalima.amb@gmail.com>
2023-03-03 13:58:23 -0300

    Fix: fixed the .Json Styled-components version, and ran the test

--- src/design-system/tooltip/tooltip.test.tsx
+++ src/design-system/tooltip/tooltip.test.tsx
@@ -9,49 +9,13 @@ import userEvent from '@testing-library/user-event';
 
 describe('<Tooltip />', async () => {
   it('should be visible when hovering the child element', async () => {
-    const { getByTestId, getByText } = render(
+    const text = 'Hi';
+    render(
       <ThemeProvider theme={theme}>
-        <Tooltip text={'oi'}>Hover me</Tooltip>
+        <Tooltip text={text}>Hover me</Tooltip>
       </ThemeProvider>,
     );
-    const popOver = getByTestId('tooltipText');
-    expect(popOver).toHaveStyle({ display: 'none' });
-    await userEvent.hover(getByTestId('tooltipWrapper'));
-    expect(getByTestId('tooltipText')).toHaveStyle({ display: 'block' });
-    //tentativa2 com .toBeVisible
-    // expect(popOver).toHaveStyle('visibility: visible');
-    // const span = await waitFor(() =>
-    //   screen.findByRole('span', { hidden: true }),
-    // );
-    // expect(span).toBeVisible;
-
-    // expect(await screen.findByText('oi')).toBeInTheDocument();
-
-    // fireEvent.mouseOver(childComponent);
-    // expect(screen.queryByTestId('tooltipText')).toContain('oi');
-    // expect(await screen.findByText('ei')).toBeInTheDocument();
-
-    //
+    await userEvent.hover(screen.getByText(/Hover me/i));
+    expect(screen.getByText(text)).toBeVisible();
   });
 });
-
-// const termsAndConditions = screen.getByText(/terms and conditions/i);
-//   userEvent.hover(termsAndConditions);
-
-//   const popover = screen.queryByText(/No ice cream will be delivered/i);
-//   expect(popover).toBeInTheDocument();
-
-//   // popover disappears when we mouse out
-//   userEvent.unhover(termsAndConditions);
-//   await waitForElementToBeRemoved(() =>
-//     screen.queryByText(/No ice cream will be delivered/i)
-//   );
-// });
-
-// popover starts out hidden
-// const nullPopover = screen.queryByText(
-//   /No ice cream will be delivered/i
-// );
-// expect(nullPopover).not.toBeInTheDocument();
-
-// popover appears upon mouseover of checkbox label
