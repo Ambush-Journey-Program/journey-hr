9ce9328b2a711d06c1991d2a338c3f83619187b2
Mariana Maia <marianalima.amb@gmail.com>
2023-03-06 18:57:41 -0300

    cleaned the app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import { GlobalStyles } from './styles/global';
-import { Tooltip } from './design-system/tooltip';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Tooltip text="Copied" children={'Hover me'}></Tooltip>
     </ThemeProvider>
   );
 }
