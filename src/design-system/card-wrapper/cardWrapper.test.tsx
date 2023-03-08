4f49d3f8692bd02f3bf742f07e2148e6ed601eb7
Mariana Maia <marianalima.amb@gmail.com>
2023-03-08 10:44:55 -0300

    refactor: add it.skip to tooltip test and delete library

--- src/design-system/card-wrapper/cardWrapper.test.tsx
+++ src/design-system/card-wrapper/cardWrapper.test.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { describe, expect } from 'vitest';
+import { theme } from '../../styles/theme';
 
 import { render, screen } from '../../tests/renderWithProviders';
 
