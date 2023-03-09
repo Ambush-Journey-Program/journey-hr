e9b73c73931359c7b800cdcef0e7da8e03c1006f
Lucas Medeiros <lucaspontoe@hotmail.com>
2023-03-09 11:51:41 -0300

    Fix: Deleted the theme provider from the Tooltip code

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -3,6 +3,10 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from '../src/styles/global';
 import { theme } from '../src/styles/theme';
 
+export const parameters = {
+  layout: 'centered',
+};
+
 const withGlobalStyle = (Story) => (
   <ThemeProvider theme={theme}>
     <GlobalStyles />
