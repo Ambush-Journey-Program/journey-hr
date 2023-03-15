547bfbbb2593b996cfe942a9101f11566ded4027
Lucas Medeiros <lucaspontoe@hotmail.com>
2023-03-15 12:49:54 -0300

    Fix: Deleted max width from Desktop and tablet

--- src/styles/devices.ts
+++ src/styles/devices.ts
@@ -1,6 +1,6 @@
 export const devices = {
   desktop: '(min-width: 1200px)',
-  tabletLarge: '(max-width: 1199px) and (min-width: 992px)',
-  tablet: '(max-width: 991px) and (min-width: 769px)',
+  tabletLarge: '(min-width: 992px)',
+  tablet: '(min-width: 769px)',
   mobile: '(max-width: 768px)',
 };
