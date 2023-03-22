7588f6f1181fa47f259db12b71ee0a613c6e18a6
Mariana Maia <marianalima.amb@gmail.com>
2023-03-22 16:29:46 -0300

    bug: token problem

--- src/design-system/calendar/api.tsx
+++ src/design-system/calendar/api.tsx
@@ -1,12 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import {
-  GoogleLogin,
-  googleLogout,
-  GoogleOAuthProvider,
-  useGoogleLogin,
-} from '@react-oauth/google';
+import { useGoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
-import jwt_decode from 'jwt-decode';
 
 export function Api() {
   const [profile, setProfile] = useState([]);
@@ -17,30 +11,39 @@ export function Api() {
   const saveToLocalStorage = (token: string) =>
     window.localStorage.setItem('google_auth_token', token);
 
-  useEffect(() => {
-    saveToLocalStorage(authToken);
-  }, [authToken]);
+  useEffect(() => {}, [authToken]);
 
   const login = useGoogleLogin({
     onSuccess: (tokenResponse) => {
       console.log(tokenResponse);
       setAuthToken(tokenResponse.access_token);
+      saveToLocalStorage(tokenResponse.access_token);
     },
   });
 
+  // `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${authToken}`,
   useEffect(() => {
     const data = axios
-      .get('https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest', {
-        headers: {
-          Authorization: `Bearer ${authToken}`,
-          Accept: 'application/json',
+      .get(
+        'https://content.googleapis.com/calendar/v3/users/me/calendarList/primary',
+        {
+          headers: {
+            authorization: `Bearer ${window.localStorage.getItem(
+              'google_auth_token',
+            )}`,
+            Accept: 'application/json',
+          },
         },
-      })
+      )
       .then((res) => {
         setProfile(res.data);
       })
       .catch((err) => console.log(err));
-  });
+  }, []);
 
-  return <button onClick={() => login()}>Sign in with Google 🚀</button>;
+  return (
+    <>
+      <button onClick={() => login()}>Sign in with Google 🚀</button>; ;
+    </>
+  );
 }
