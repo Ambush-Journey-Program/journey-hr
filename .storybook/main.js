b46ad3c8b7c21ed16931946beb1f213b1791dc9c
Mariana Maia <marianalima.amb@gmail.com>
2023-03-28 11:26:14 -0300

    Fix: congifured the alias path to work on storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,5 @@
 const svgrPlugin = require('vite-plugin-svgr');
+const path = require('path');
 
 module.exports = {
   async viteFinal(config, { configType }) {
@@ -13,7 +14,17 @@ module.exports = {
     ];
 
     // return the customized config
-    return config;
+    return {
+      ...config,
+      resolve: {
+        alias: [
+          {
+            find: '@',
+            replacement: path.resolve(__dirname, './src'),
+          },
+        ],
+      },
+    };
   },
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
