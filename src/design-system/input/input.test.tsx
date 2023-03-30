4dcafd125d2c45afca57695476dc1a3b4094bf9b
Lucas Medeiros <116045026+MedeirosKalluh@users.noreply.github.com>
2023-03-30 16:35:17 -0300

    Feat: implemented the test and storybook components for default input (#53)
    ## Briefing
    
    implemented the test and storybook components for default input
    
    
    ## Achievements
    
    
    - Create storybook file for default input
    
    - Create test file for default input
    
    https://ambush-journey.atlassian.net/jira/software/projects/AJPHR/boards/1?selectedIssue=AJPHR-303
    
    ## Screenshot
    
    Test
    
    ![Screen Shot 2023-03-29 at 19 50 52](https://user-images.githubusercontent.com/116045026/228689941-900d4ff0-e074-45a8-914c-e03ba39074d8.png)
    
    
    StoryBook
    
    https://user-images.githubusercontent.com/116045026/228689910-973f3638-19b1-4f89-8add-d0b216c3bb81.mov

---
+++ src/design-system/input/input.test.tsx
@@ -0,0 +1,26 @@
+import { describe, expect } from 'vitest';
+import { render, screen } from '@/src/../tests/renderWithProviders';
+import { Input } from './input';
+
+describe('<Input />', () => {
+  it('renders the Input', () => {
+    render(<Input label="label" />);
+    const inputEl = screen.getByTestId('input-test');
+    expect(inputEl).toBeInTheDocument();
+  });
+
+  it('could be disabled', () => {
+    render(<Input label="label" disabled />);
+
+    const inputEl = screen.getByTestId('input-test');
+    expect(inputEl).toBeDisabled();
+    expect(inputEl).not.toBeEnabled();
+  });
+
+  it('could be required', () => {
+    render(<Input label="label" required />);
+
+    const inputEl = screen.getByTestId('input-test');
+    expect(inputEl).toBeRequired();
+  });
+});
