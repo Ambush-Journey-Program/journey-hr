a007d6189bdd75124445e228bca9726e76a5ba86
BarbaraSantos <barbarasantos@Poas-MBP.(none)>
2023-04-03 15:16:57 -0300

    Fix : fix test

--- src/components/ui/back-button/back-button.test.tsx
+++ src/components/ui/back-button/back-button.test.tsx
@@ -6,13 +6,13 @@ describe('<BackButton />', () => {
   describe('should display Back button', () => {
     it('The button is not disabled and clickable', () => {
       const onClickMock = vi.fn();
-      render(<BackButton disabled={true} onClick={onClickMock} />);
+      render(<BackButton onClick={onClickMock} />);
       expect(screen.getByText('Back')).not.toBeDisabled();
     });
 
     it('The button is disabled and unclickable', () => {
       const onClickMock = vi.fn();
-      render(<BackButton disabled={false} onClick={onClickMock} />);
+      render(<BackButton disabled onClick={onClickMock} />);
       const backButton = screen.getByText('Back');
       userEvent.click(backButton);
       expect(backButton).toBeDisabled();
