473eac3a772ab47a982d30616655d4922f97fac5
BarbaraSantos <barbarasantos@Poas-MBP.(none)>
2023-04-03 14:18:43 -0300

    Feat: create styled wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import { GlobalStyles } from './styles/global';
+import { ShortHappyHourWrapper } from './design-system/short-happy-hour-wrapper';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
+      <ShortHappyHourWrapper
+        label="name of the event"
+        children={'hihi'}
+      ></ShortHappyHourWrapper>
     </ThemeProvider>
   );
 }
--- src/design-system/short-happy-hour-wrapper/short-happy-hour-wrapper.styled.ts
+++ src/design-system/short-happy-hour-wrapper/short-happy-hour-wrapper.styled.ts
@@ -1,3 +1,26 @@
-import styled from 'styled-components'
+import { devices } from '@/styles/devices';
+import styled from 'styled-components';
 
-export const Wrapper = styled.div``
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 12px;
+  gap: 18px;
+  padding: 12px;
+  padding-bottom: 23px;
+  border-radius: 10px;
+  width: 100%;
+  max-width: 100%;
+  background-color: ${(props) => props.theme.color.contrasts.lightContrast};
+
+  @media ${devices.tabletLarge} {
+    flex-direction: row;
+    gap: 50px;
+    padding-bottom: 22px;
+  }
+
+  @media ${devices.tablet}, ${devices.desktop} {
+    padding-bottom: 20px;
+  }
+`;
