924cf703ae189bb36470896c23106c3b499c0b75
BarbaraSantos <barbarasantos@Poas-MBP.(none)>
2023-04-06 11:56:01 -0300

    Feat: change the px on tablet and mobile devices.

--- src/styles/devices.ts
+++ src/styles/devices.ts
@@ -1,6 +1,6 @@
 export const devices = {
   desktop: '(min-width: 1200px)',
   tabletLarge: '(min-width: 992px)',
-  tablet: '(min-width: 769px)',
-  mobile: '(max-width: 768px)',
+  tablet: '(min-width: 768px)',
+  mobile: '(max-width: 767px)',
 };
