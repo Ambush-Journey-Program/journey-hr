13be7b11435b601eca29620fb99d3877c39cdcd5
BarbaraSantos <barbarasantos@Poas-MBP.(none)>
2023-04-10 17:20:44 -0300

    Feat: creted tests

--- src/components/happy-hour-edit/happy-hour-edit.test.tsx
+++ src/components/happy-hour-edit/happy-hour-edit.test.tsx
@@ -1,18 +1,47 @@
-import { render, screen } from '@/tests/renderWithProviders';
 import * as React from 'react';
+import { render, screen } from '@/tests/renderWithProviders';
 import { HappyHourEdit } from '../happy-hour-edit';
 import { guestsList } from './const';
+import { Guest } from './types';
 
 describe('<HappyHourEdit />', () => {
   describe('should have this remove', () => {
-    it('when the component is actually used', () => {
+    it('It should have a title', () => {
+      function addGuest(updatedGuestList: Guest[]) {}
       render(
-        <HappyHourEdit guestsList={guestsList} maxGuests={6} {...props} />,
+        <HappyHourEdit
+          guestsList={guestsList}
+          maxGuests={6}
+          addGuest={addGuest}
+        />,
       );
-      const interviewsubtitle = screen.getByText(
-        'Schedule a technical interview for a candidate.',
+      const title = screen.getByText('Group Names');
+      expect(title).toBeInTheDocument();
+    });
+    it('It should have a Select input', () => {
+      function addGuest(updatedGuestList: Guest[]) {}
+      render(
+        <HappyHourEdit
+          guestsList={guestsList}
+          maxGuests={6}
+          addGuest={addGuest}
+        />,
       );
-      expect(interviewsubtitle).toBeInTheDocument();
+      const title = screen.getByText('Group Names');
+      expect(title).toBeInTheDocument();
+    });
+
+    it('It should have a maxGuest', () => {
+      function addGuest(updatedGuestList: Guest[]) {}
+      render(
+        <HappyHourEdit
+          guestsList={guestsList}
+          maxGuests={6}
+          addGuest={addGuest}
+        />,
+      );
+      expect(guestsList).to.have.length(4);
+      expect(guestsList.length).toBeLessThanOrEqual(6);
     });
   });
 });
