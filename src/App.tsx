9ef3fbbf961b4687e13b46a165cdd342dea3d03f
Lucas Medeiros <lucaspontoe@hotmail.com>
2023-04-26 16:32:38 -0300

    Fix: deleted component from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import { GlobalStyles } from './styles/global';
-import { TextAreaRequestReason } from './components/request-time-off/time-of-reason-text-area/reason-text-area';
-import { CardWrapper } from './design-system';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyles />{' '}
-      <CardWrapper>
-        <TextAreaRequestReason />
-      </CardWrapper>
+      <GlobalStyles />
     </ThemeProvider>
   );
 }
