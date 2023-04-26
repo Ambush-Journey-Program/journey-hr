48fc2ea2a9331989afd0de4d4bdb01b78abef1ac
BarbaraSantos <barbarasantos@Poas-MBP.(none)>
2023-04-26 15:38:13 -0300

    Feat: responsiveness was made.

--- src/design-system/time-off-type/time-off-type.styled.ts
+++ src/design-system/time-off-type/time-off-type.styled.ts
@@ -1,8 +1,19 @@
 import styled from 'styled-components';
+import { devices } from '@/styles/devices';
 
 export const Wrapper = styled.div`
   gap: 16px;
 `;
-export const DivParagraph = styled.div`
-  margin-bottom: 20px;
+
+export const ParagraphStyle = styled.p`
+  font-size: 16px;
+  margin-bottom: 16px;
+  font-weight: 500;
+  line-height: 24px;
+  font-family: ${(props) => props.theme.font.fontFamilyTitle};
+  color: ${(props) => props.theme.color.contrasts.highContrast};
+  @media ${devices.tablet} {
+    font-size: 18px;
+    line-height: 27px;
+  }
 `;
--- src/design-system/time-off-type/time-off-type.tsx
+++ src/design-system/time-off-type/time-off-type.tsx
@@ -1,15 +1,10 @@
 import { RadioButton } from '../radio-button';
-import { Paragraphs } from '../typography';
 import * as Styled from './time-off-type.styled';
 
 export function TimeOffType() {
   return (
     <Styled.Wrapper>
-      <Styled.DivParagraph>
-        <Paragraphs size={'medium'} fontWeight={'semibold'}>
-          Aplly For
-        </Paragraphs>
-      </Styled.DivParagraph>
+      <Styled.ParagraphStyle>Aplly For</Styled.ParagraphStyle>
       <RadioButton
         defaultChecked
         value={'paid-time-off'}
