5a6d61b6c98f87dc9229028f82fe3b39b761ec0a
Mariana Maia <marianalima.amb@gmail.com>
2023-05-10 15:32:59 -0300

    Fix: turned the balance flexible

--- src/components/balance/balance.test.tsx
+++ src/components/balance/balance.test.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { Balance } from '..';
+import { Balance } from '.';
 import { render, screen } from '@/tests/renderWithProviders';
 
 describe('<Balance />', () => {
@@ -8,19 +8,45 @@ describe('<Balance />', () => {
     const label = 'Your Current Balance';
     const props = { label };
 
-    render(<Balance isAdmin={false} availableDays={0} {...props} />);
+    render(
+      <Balance
+        consumedDays={0}
+        nextRefill={''}
+        optionalHolidays={0}
+        isAdmin={false}
+        availableDays={0}
+        {...props}
+      />,
+    );
     const labelSpan = screen.getByText(label);
 
     expect(labelSpan).toHaveTextContent(label);
   });
 
   it('It checks if it is a admin card', () => {
-    render(<Balance isAdmin={true} availableDays={0} />);
+    render(
+      <Balance
+        isAdmin={true}
+        availableDays={0}
+        consumedDays={0}
+        nextRefill={''}
+        optionalHolidays={0}
+      />,
+    );
     const avatar = screen.getByRole('img');
     expect(avatar).toBeVisible();
   });
   it('It checks if the button is visible', () => {
-    render(<Balance isAdmin={true} availableDays={0} buttonClick={() => {}} />);
+    render(
+      <Balance
+        isAdmin={true}
+        availableDays={0}
+        buttonClick={() => {}}
+        consumedDays={0}
+        nextRefill={''}
+        optionalHolidays={0}
+      />,
+    );
     const button = screen.getByRole('button');
     expect(button).toBeVisible();
   });
