045c9778e780fe67c42cd39c418d1f94fe3db1ff
Marcello <marcello_gp_@hotmail.com>
2023-05-11 15:59:17 -0300

    removed git code from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,11 @@
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import { GlobalStyles } from './styles/global';
-import { Button } from './design-system';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Button icon="AcademicCapIcon"></Button>
-      <Button
-        icon="AdjustmentsVerticalIcon"
-        color="alternative"
-        ariaLabel="teste"
-      >
-        teste
-      </Button>
     </ThemeProvider>
   );
 }
