fc9b99d41cff2fcbe114ae1bf84a9737a4725978
Mariana Maia <marianalima.amb@gmail.com>
2023-05-17 14:04:29 -0300

    Fixed Paragraph typos

--- src/pages/select-employee/components/employee-not-found.tsx
+++ src/pages/select-employee/components/employee-not-found.tsx
@@ -1,13 +1,13 @@
 /* eslint-disable react/no-unescaped-entities */
-import { Paragraphs } from '@/design-system';
+import { Paragraph } from '@/design-system';
 import * as Styled from './employee-not-found-styled';
 export function EmployeeNotFound() {
   return (
     <Styled.Span>
-      <Paragraphs size="extrasmall" fontWeight="light" colorVariant="red">
+      <Paragraph size="extrasmall" fontWeight="light" colorVariant="red">
         We could't find anyone with this name. Check your spelling or try a
         different name.
-      </Paragraphs>
+      </Paragraph>
     </Styled.Span>
   );
 }
