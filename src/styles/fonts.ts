59e19725b12482c1971b75549bedc4e49c9bea8f
BarbaraSantos <barbarasantos@Poas-MBP.(none)>
2023-05-19 11:15:57 -0300

    Feat: started to implement the font and test then.

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,3 +1,7 @@
+// @import url('https://use.typekit.net/dta1xjt.css');
+
+// import "./fonts/Poppins/Poppins-Regular.ttf";
+
 export const font = {
   fontFamilyTitle: "'Poppins', sans-serif",
   fontFamilyBody: "'Mundial', sans-serif",
