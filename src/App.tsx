793f52eeb578c436e76474f0ff6795e653edfba4
BarbaraSantos <barbarasantos@Poas-MBP.(none)>
2023-05-22 16:29:59 -0300

    Feat: APP.tsx was cleaned.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import { GlobalStyles } from './styles/global';
-import { SelectPeriod } from './components/select-period';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SelectPeriod></SelectPeriod>
     </ThemeProvider>
   );
 }
