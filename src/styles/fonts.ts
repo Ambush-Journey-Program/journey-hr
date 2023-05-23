77f3713db3499b1b3798e88b221c126f3990258a
BarbaraSantos <barbarasantos@Poas-MBP.(none)>
2023-05-23 10:19:37 -0300

    Feat:the fonts.ts was cleaned.

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -1,7 +1,3 @@
-// @import url('https://use.typekit.net/dta1xjt.css');
-
-// import "./fonts/Poppins/Poppins-Regular.ttf";
-
 export const font = {
   fontFamilyTitle: "'Poppins', sans-serif",
   fontFamilyBody: "'Mundial', sans-serif",
