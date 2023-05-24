af29cf657824221e4853532f738af82798e17316
Lucas Medeiros <lucaspontoe@hotmail.com>
2023-05-24 18:04:10 -0300

    Fix: Deleted app lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import { GlobalStyles } from './styles/global';
-import { RequestTimeOffContent } from './components/request-time-off';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <RequestTimeOffContent />
     </ThemeProvider>
   );
 }
