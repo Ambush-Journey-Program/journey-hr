4fc767e2a7f2ce14f5038777cabe466d80224217
Ralph Effting <ralfting@gmail.com>
2023-05-30 09:24:33 -0300

    Fix missing spaces

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,7 @@
 import { contrasts, auxiliary, gradients, brandColors, button } from './colors';
 import { font } from './fonts';
 import { shadows } from './shadows';
+import { spacings } from './spacing';
 
 export const theme = {
   color: {
@@ -11,5 +12,6 @@ export const theme = {
     button,
   },
   shadows,
-  font: font,
+  font,
+  spacings,
 };
