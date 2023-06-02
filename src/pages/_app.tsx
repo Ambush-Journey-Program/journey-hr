18c168ffa4bce384a17b0066531dcedd6103afd6
Marcello <marcello_gp_@hotmail.com>
2023-06-02 11:09:28 -0300

    imported global-reset css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
+import '../styles/global.css';
 
 import StyledComponentsRegistry from '@/lib/registry';
 import { theme } from '@/styles/theme';
+import { Button } from '@/design-system';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
--- src/styles/global.ts
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from 'styled-components';
-
-import './reset.css';
-
-export const GlobalStyles = createGlobalStyle`
-  body, input, button, select, span {
-    font-family: Arial, Helvetica, sans-serif;
-  }
-`;
