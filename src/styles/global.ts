d5edb146d6875ecac9754a074f61b92ea3d8e853
Marcello <marcello_gp_@hotmail.com>
2023-06-02 11:25:51 -0300

    imported global-reset css

---
+++ src/styles/global.ts
@@ -0,0 +1,9 @@
+import { createGlobalStyle } from 'styled-components';
+
+import './reset.css';
+
+export const GlobalStyles = createGlobalStyle`
+  body, input, button, select, span {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+`;
