ee7edc6bd30d83b30692e98330ecb66392298cef
Marcello <32502447+MarcelloAragoni@users.noreply.github.com>
2023-06-02 12:00:34 -0300

    AJPHR-418 - imported global-reset css (#120)
    ### Briefing
    Ticket: https://ambush-journey.atlassian.net/browse/AJPHR-418
    
    ### Summary
    Imported reset/global css
    
    ### Screenshot
    <img width="740" alt="image" src="https://github.com/Ambush-Journey-Program/journey-hr/assets/32502447/c68f36ca-d2fc-4892-9db2-89bcda37e7f3">

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
+import '../styles/global.css';
 
 import StyledComponentsRegistry from '@/lib/registry';
 import { theme } from '@/styles/theme';
