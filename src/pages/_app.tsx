6ec70e9facd533ded91b8a610dd68804293b8a29
Marcello <marcello_gp_@hotmail.com>
2023-06-02 11:21:02 -0300

    imported global-reset css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import '../styles/global.css';
 
 import StyledComponentsRegistry from '@/lib/registry';
 import { theme } from '@/styles/theme';
-import { Button } from '@/design-system';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
