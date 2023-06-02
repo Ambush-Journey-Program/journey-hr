30dd3f5cd4b6b092987f3c4efcc944fef85ddeb8
Mariana Lima <marianalima.amb@gmail.com>
2023-06-02 20:45:22 -0300

    Fixed images bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import { Inter } from 'next/font/google';
+import { Avatar, Doodles, NavBar } from '@/design-system';
+import { Icon } from '@/design-system/icon/icon';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,6 +14,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Avatar src={'/static/images/UserPhoto.png'} />
+      <NavBar navigationLinks={[]} colors={'dark'} />
+      <Doodles icon={'arrow1'} colors={'dark'} />
+      <Doodles icon={'spinningArrow'} colors={'dark'} />
+      <Icon icon="TvIcon" size="58px" color={'error'} />
     </>
   );
 }
