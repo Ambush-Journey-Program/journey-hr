d72ab576e99f56bc628614a558c8f0c44a2501e2
Mariana Lima <marianalima.amb@gmail.com>
2023-06-06 00:18:45 -0300

    implement args in the radioButton stories

--- src/design-system/radio-button/radio-button.stories.tsx
+++ src/design-system/radio-button/radio-button.stories.tsx
@@ -1,4 +1,4 @@
-import { Meta, Story } from '@storybook/react';
+import { Meta, Story, StoryFn } from '@storybook/react';
 import { RadioButton } from './radio-button';
 import { RadioButtonProps } from './types';
 
@@ -8,35 +8,46 @@ export default {
   parameters: {},
 } as Meta;
 
-const Template: Story<RadioButtonProps> = (args) => {
-  return (
-    <>
-      <RadioButton
-        defaultChecked={true}
-        label={'Selection 1'}
-        value={'Selection 1'}
-        name={'Selection 1'}
-      />
-      <RadioButton
-        defaultChecked={false}
-        label={'Selection 2'}
-        value={'Selection 2'}
-        name={'Selection 1'}
-      />
-      <RadioButton
-        defaultChecked={false}
-        label={'Selection 3'}
-        value={'Selection 3'}
-        name={'Selection 1'}
-      />
-      <RadioButton
-        defaultChecked={false}
-        label={'Selection 4'}
-        value={'Selection 4'}
-        name={'Selection 1'}
-      />
-    </>
-  );
+export const RadioButtonStory: StoryFn<typeof RadioButton> = (
+  args: JSX.IntrinsicAttributes & RadioButtonProps,
+) => (
+  <>
+    <RadioButton
+      defaultChecked={true}
+      label={'Selection 1'}
+      value={'Selection 1'}
+      name={'Selection 1'}
+    />
+    <RadioButton
+      defaultChecked={false}
+      label={'Selection 2'}
+      value={'Selection 2'}
+      name={'Selection 1'}
+    />
+    <RadioButton
+      defaultChecked={false}
+      label={'Selection 3'}
+      value={'Selection 3'}
+      name={'Selection 1'}
+    />
+    <RadioButton
+      defaultChecked={false}
+      label={'Selection 4'}
+      value={'Selection 4'}
+      name={'Selection 1'}
+    />
+  </>
+);
+
+RadioButtonStory.args = {
+  fontWeight: 'hair',
+  disabled: false,
+  defaultChecked: false,
 };
 
-export const RadioButtonStory = Template.bind({});
+RadioButtonStory.argTypes = {
+  fontWeight: {
+    control: { type: 'radio' },
+    options: ['hair', 'semibold'],
+  },
+};
