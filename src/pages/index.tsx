bbc7adf109cd720c16bae24c3dbcfd662d735d2f
Marcello <marcello_gp_@hotmail.com>
2023-06-15 12:29:15 -0300

    Refinements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheet" href="https://use.typekit.net/dta1xjt.css"/>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
       </Head>
       <Input required label={'Mans worlds'}/>
     </>
