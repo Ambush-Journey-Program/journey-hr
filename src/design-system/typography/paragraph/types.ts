903ac498978dc3e15b9cbc4f7f6b597dff52e6eb
BarbaraSantos <barbarasantos@Poas-MBP.(none)>
2023-06-20 10:18:34 -0300

    Feat: the "" and type adjustments were made.

--- src/design-system/typography/paragraph/types.ts
+++ src/design-system/typography/paragraph/types.ts
@@ -19,7 +19,7 @@ export type ColorType =
   | 'error';
 
 export type ParagraphProps = {
-  children: string;
+  children: Text;
   size: ParagraphType;
   fontWeight: TextWeight;
   colorVariant?: ColorType;
