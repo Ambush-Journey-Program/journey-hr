1243b0f7c32e60bf8d81bdf5ab8e9070226baa8f
BarbaraSantos <barbarasantos@Poas-MBP.(none)>
2023-06-26 10:13:22 -0300

    Feat: Icon and Title was styled.

--- src/pages/request-successful/requestSuccessful.styled.ts
+++ src/pages/request-successful/requestSuccessful.styled.ts
@@ -2,7 +2,17 @@ import styled from "styled-components";
 
 export const ContainerTitle = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin: 20px 0 60px 0;
+  gap: ${(props) => props.theme.spacings.spacing02};
+  margin: ${(props) => props.theme.spacings.spacing04} 0 ${(props) => props.theme.spacings.spacing08} 0;
+`;
+
+export const IconContainer = styled.div`
+  display: flex;
+  box-sizing: border-box;
+  justify-content: center;
+  align-items: center;
+  margin:${(props) => props.theme.spacings.spacing04} 0 ${(props) => props.theme.spacings.spacing04} 0;
 `;
--- src/pages/request-successful/requestSuccessful.tsx
+++ src/pages/request-successful/requestSuccessful.tsx
@@ -1,19 +1,29 @@
-import { BackButton, Balance, Footer, PageHeader, Summary } from "@/components";
+import { BackButton, Balance, Footer, Summary } from "@/components";
 import * as Styled from './requestSuccessful.styled';
-import { EditButton } from "@/components/summary/summarybuttons/editButtons";
-import { DeleteButton } from "@/components/summary/summarybuttons/deleteButtons";
+import { Icon, Subtitle, Title } from "@/design-system";
+import { useRouter } from "next/router";
 
 export function RequestSuccessful() {
+  const router = useRouter();
+  const handleNavigate = () => {
+    router.push('/');
+  }
   return (
     <>
       <div>
         <BackButton onClick={function (): void {}} />
+        <Styled.IconContainer>
+          <Icon color="accepted" icon='CheckCircleIcon' size="58px"/>
+        </Styled.IconContainer>
         <Styled.ContainerTitle>
-          <PageHeader title={"All done!"} subtitle={"Your Time Off was requested, cheers!"}></PageHeader>
+        <Title variant="h4">{"All done!"}</Title>
+          <Subtitle fontWeight="regular" variant="s6">
+            {"Your Time Off was requested, cheers!"}
+          </Subtitle>
         </Styled.ContainerTitle>
         <Summary variant={"manager"} display={false}></Summary>
         <Balance isAdmin={false} availableDays={0} consumedDays={0} optionalHolidays={0}></Balance>
-        <Footer variant={"allDone"}></Footer>
+        <Footer variant={"allDone"} onApply={handleNavigate}></Footer>
       </div>
     </>
   )
